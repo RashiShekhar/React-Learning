f2f45035ce47de5df30841aca7b196a9419821fc
RashiShekhar <rashi1shekhar@email.com>
2024-08-31 22:12:21 +0530

    File

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -4,14 +4,14 @@ export default function Example() {
   const [position, setPosition] = useState(0);
 
   var Positionclass = " ";
-  if (Positionclass === 0) {
-    setPosition("left-[10]");
-  } else if (Positionclass === 1) {
-    setPosition("right: 0");
-  } else if (Positionclass === 2) {
-    setPosition("top:0");
-  } else if (Positionclass === 3) {
-    setPosition("bottom:0");
+  if (position === 0) {
+    Positionclass = "left-0";
+  } else if (position === 1) {
+    Positionclass = "right-0";
+  } else if (position === 2) {
+    Positionclass = "top-0";
+  } else if (position === 3) {
+    Positionclass = "bottom-0";
   }
 
   /* 0 =left
@@ -24,20 +24,24 @@ export default function Example() {
     <div>
       <div className={`relative border-2 border-black w-60 h-60 `}>
         <div
-          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${Positionclass} transition-all duration-700 `}
+          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${position} transition-all duration-700 `}
         ></div>
         <div className=" flex gap-3 ">
           <button
             className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white  "
-            onClick={() => {}}
+            onClick={() => setPosition()}
           >
             Left
           </button>
           <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white ">
             Right
           </button>
-          <button>Top</button>
-          <button>Bottom</button>
+          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white">
+            Top
+          </button>
+          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white">
+            Bottom
+          </button>
         </div>
       </div>
     </div>
