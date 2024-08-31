21a035168c92464a4fac80715ca85784973ea24c
RashiShekhar <rashi1shekhar@email.com>
2024-08-31 21:35:00 +0530

    Files

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 export default function Example() {
   const [position, setPosition] = useState(0);
 
-  var Position = " ";
-  if (Position === 0) {
-    setPosition("left = 0");
-  } else if (Position === 1) {
-    setPosition("right = 0");
-  } else if (Position === 2) {
-    setPosition("top = 0");
-  } else if (Position === 3) {
-    setPosition("bottom = 0");
+  var Positionclass = " ";
+  if (Positionclass === 0) {
+    setPosition("left-[10]");
+  } else if (Positionclass === 1) {
+    setPosition("right: 0");
+  } else if (Positionclass === 2) {
+    setPosition("top:0");
+  } else if (Positionclass === 3) {
+    setPosition("bottom:0");
   }
 
   /* 0 =left
@@ -24,16 +24,18 @@ export default function Example() {
     <div>
       <div className={`relative border-2 border-black w-60 h-60 `}>
         <div
-          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${Position} transition-all duration-700 `}
+          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${Positionclass} transition-all duration-700 `}
         ></div>
         <div className=" flex gap-3 ">
           <button
-            className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white "
-            onClick={() => setPosition()}
+            className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white  "
+            onClick={() => {}}
           >
             Left
           </button>
-          <button>Right</button>
+          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white ">
+            Right
+          </button>
           <button>Top</button>
           <button>Bottom</button>
         </div>
