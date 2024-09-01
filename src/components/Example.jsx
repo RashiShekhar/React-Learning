4e22e46d1f04c060debbc7866445d66ad119fdde
RashiShekhar <rashi1shekhar@email.com>
2024-09-01 14:24:31 +0530

    Examplefiles

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -3,54 +3,37 @@ import React, { useState } from "react";
 export default function Example() {
   const [position, setPosition] = useState(0);
 
-  var Positionclass = " ";
-  if (position === 0) {
-    Positionclass = "left-0 left-[205px]  ";
-  } else if (position === 1) {
-    Positionclass = "top-0 top-[]";
-  } else if (position === 2) {
-    Positionclass = "right-0 top-0 ";
-  } else if (position === 3) {
-    Positionclass = " left-0 left-30 ";
-  }
-
-  /* 0 =left
-     1=right
-     2= top
-     3=bottom
-  */
-
   return (
     <div>
-      <div className={`relative border-2 border-black w-60 h-60 `}>
+      <div className="relative border-2 border-black w-60 h-60">
         <div
-          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${Positionclass}`}
+          className={`absolute border rounded-full bg-black w-6 h-6  ${position} transition-all duration-700`}
         ></div>
       </div>
-      <div className=" flex gap-3 ">
+      <div>
         <button
-          className=" border rounded-md bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-700  "
-          onClick={() => setPosition(0)}
-        >
-          Left Bottom
-        </button>
-        <button
-          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-900 "
-          onClick={() => setPosition(1)}
-        >
-          bottom Right
-        </button>
-        <button
-          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-900 "
-          onClick={() => setPosition(2)}
-        >
-          Right Top
-        </button>
-        <button
-          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-900 "
-          onClick={() => setPosition(3)}
+          className="border rounded-xl px-3 py-2 bg-black text-white"
+          onClick={() => setPosition("top-0 left-0")}
         >
           Top Left
+        </button>
+        <button
+          className="border rounded-xl px-3 py-2 bg-black text-white"
+          onClick={() => setPosition("left-[90%] top-0")}
+        >
+          Top Right
+        </button>
+        <button
+          className="border rounded-xl px-3 py-2 bg-black text-white"
+          onClick={() => setPosition("top-[90%] left-0")}
+        >
+          Bottom Left
+        </button>
+        <button
+          className="border rounded-xl px-3 py-2 bg-black text-white"
+          onClick={() => setPosition("top-[90%] left-[90%]")}
+        >
+          Bottom Right
         </button>
       </div>
     </div>
