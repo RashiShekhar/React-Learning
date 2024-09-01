421445b965904852f722edac231e598ec334d642
RashiShekhar <rashi1shekhar@email.com>
2024-09-01 07:57:00 +0530

    example

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -24,25 +24,34 @@ export default function Example() {
     <div>
       <div className={`relative border-2 border-black w-60 h-60 `}>
         <div
-          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${position} transition-all duration-700 `}
+          className={`absolute border rounded-full bg-black w-8 h-8 items-center justify-center ${position}`}
         ></div>
-        <div className=" flex gap-3 ">
-          <button
-            className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white  "
-            onClick={() => setPosition()}
-          >
-            Left
-          </button>
-          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white ">
-            Right
-          </button>
-          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white">
-            Top
-          </button>
-          <button className="border rounder-md bg-gray-300 px-3 py-2 text-white hover:bg-white hover:text-white">
-            Bottom
-          </button>
-        </div>
+      </div>
+      <div className=" flex gap-3 ">
+        <button
+          className=" border rounded-md bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-300  "
+          onClick={() => setPosition(0)}
+        >
+          Left
+        </button>
+        <button
+          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-300 "
+          onClick={() => setPosition(1)}
+        >
+          Right
+        </button>
+        <button
+          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-300 "
+          onClick={() => setPosition(2)}
+        >
+          Top
+        </button>
+        <button
+          className="border rounded-md  bg-gray-400 px-3 py-2 text-white hover:bg-black hover:text-white transition-all duration-300 "
+          onClick={() => setPosition(3)}
+        >
+          Bottom
+        </button>
       </div>
     </div>
   );
